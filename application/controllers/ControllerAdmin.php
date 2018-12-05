cedc4ff51c288dcf59d401acc9cfcf2449497148
Alvinanda Rosha Kurniawan <alvinandakurniawan@yahoo.co.id>
2018-12-05 17:16:49 +0700

    update controlleradmin
    admin sudah langsung login

--- application/controllers/ControllerAdmin.php
+++ application/controllers/ControllerAdmin.php
@@ -1,83 +1,221 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class ControllerAdmin extends CI_Controller {
+class ControllerMember extends CI_Controller {
 
 	public function __construct(){
     parent::__construct();
     $this->load->model('member');
-    $this->load->model('iklan');
-    $this->load->model('komentar');
+		$this->load->model('pesan');
     $this->load->helper('url_helper');
 		$this->load->helper(array('form', 'url'));
 		$this->load->library('form_validation');
   }
 
-  public function loginAdmin(){
-    $this->form_validation->set_rules('email', 'Email', 'required');
-    $this->form_validation->set_rules('pass', 'Password', 'required');
+	public function registrasi(){
+		if($this->session->userdata('status') === "login"){
+		  $this->load->view('header');
+		  $this->load->view('errorSudahLogin');
+		  $this->load->view('footer');
+		}else{
+			$this->form_validation->set_rules('nama', 'Nama', 'required');
+			$this->form_validation->set_rules('email', 'Email', 'required');
+			$this->form_validation->set_rules('pass', 'Password', 'required');
+			$this->form_validation->set_rules('alamat', 'Alamat', 'required');
+			$this->form_validation->set_rules('telp', 'Telepon', 'required');
+			$this->form_validation->set_rules('deskripsi', 'deskripsi', 'required');
+			$this->form_validation->set_message('required', '<div class="alert alert-danger">%s tidak boleh kosong !</div>');
 
-    if($this->form_validation->run() === FALSE){
-      $this->load->view('header');
-      $this->load->view('admin/login');
-      $this->load->view('footer');
-    }else{
-      $email = $this->input->post('email');
-      $password = $this->input->post('pass');
+			if($this->form_validation->run() === FALSE){
+				$this->load->view('header');
+				$this->load->view('member/registrasi');
+				$this->load->view('footer');
+			}else{
+				//cek email sudah ada apa belum
+				$cek = $this->member->login("member",array('email'=> $this->input->post('email')))->num_rows();
+				if($cek>0){
+					$this->load->view('header');
+					$this->load->view('errorEmailSudahAda');
+					$this->load->view('footer');
+				}else{
+					$config['upload_path'] = './foto/';
+					$config['allowed_types'] = 'gif|jpg|png|jpeg';
+					$config['max'] = 0;
+					$this->load->library('upload', $config);
+					if (!$this->upload->do_upload('foto')){
+		        echo $this->upload->display_errors('<p>', '</p>');
+		    	}else{
+						$this->member->createMember($this->upload->data('file_name'),$this->input->post());
+						redirect(base_url('login'));
+					}
+				}
+			}
+		}
+	}
 
-      if($email === "admin" && $password === "admin"){
-        $data_session = array(
-					'nama' => "ADMIN",
-					'status' => "login"
+	public function editProfil(){
+		if($this->session->userdata('status') != "login"){
+		  $this->load->view('header');
+		  $this->load->view('errorBelumLogin');
+		  $this->load->view('footer');
+		}else{
+			$this->form_validation->set_rules('nama', 'Nama', 'required');
+			$this->form_validation->set_rules('email', 'Email', 'required');
+			$this->form_validation->set_rules('pass', 'Password', 'required');
+			$this->form_validation->set_rules('alamat', 'Alamat', 'required');
+			$this->form_validation->set_rules('telp', 'Telepon', 'required');
+			$this->form_validation->set_rules('deskripsi', 'deskripsi', 'required');
+			$this->form_validation->set_message('required', '<div class="alert alert-danger">%s tidak boleh kosong !</div>');
+
+			$id = $this->session->userdata('id');
+			$data['dataProfil'] = $this->member->getData(FALSE, $id);
+			$data['notifikasi'] = $this->jumlahNotifikasi();
+
+			if($this->form_validation->run() === FALSE){
+				$this->load->view('header');
+				$this->load->view('member/editProfil', $data);
+				$this->load->view('footer');
+			}else{
+				$cek = $this->member->login("member",array('email'=> $this->input->post('email')))->num_rows();
+				if($cek>0 && $this->input->post('email') != $this->session->userdata('email')){
+					$this->load->view('header');
+					$this->load->view('errorEmailSudahAda');
+					$this->load->view('footer');
+				}else{
+					$config['upload_path'] = './foto/';
+					$config['allowed_types'] = 'gif|jpg|png|jpeg';
+					$config['max'] = 0;
+					$this->load->library('upload', $config);
+					if (!$this->upload->do_upload('foto')){
+		        echo $this->upload->display_errors('<p>', '</p>');
+		    	}else{
+						$data_session = array(
+							'nama' => $this->input->post('nama'),
+							'status' => "login",
+							'id' => $id,
+							'email' => $this->input->post('email'),
+							'alamat' => $this->input->post('alamat'),
+							'deskripsi' => $this->input->post('deskripsi'),
+							'telp' => $this->input->post('telp'),
+							'foto' => "foto/".$this->upload->data('file_name')
+							);
+						$this->session->set_userdata($data_session);
+						$this->member->editData($this->upload->data('file_name'),$id,$this->input->post());
+			      redirect('profil');
+					}
+				}
+			}
+		}
+	}
+
+	public function login(){
+		if($this->session->userdata('status') == "login"){
+		  $this->load->view('header');
+		  $this->load->view('errorSudahLogin');
+		  $this->load->view('footer');
+		}else{
+			$this->form_validation->set_rules('nama', 'Username', 'required');
+			$this->form_validation->set_rules('pass', 'Password', 'required');
+			$this->form_validation->set_message('required', '<div class="alert alert-danger">%s tidak boleh kosong !</div>');
+			if($this->form_validation->run() === FALSE){
+				$this->load->view('header');
+				$this->load->view('member/login');
+				$this->load->view('footer');
+			}else{
+				$username = $this->input->post('nama');
+				$password = $this->input->post('pass');
+				$where = array(
+					'nama' => $username,
+					'password' => md5($password)
 					);
-				$this->session->set_userdata($data_session);
-				redirect('admin/kelolaSistem');
-      }else{
-        echo "Email dan Password tidak cocok dengan email password admin";
-        $this->load->view('header');
-        $this->load->view('admin/login');
-        $this->load->view('footer');
-      }
-    }
-  }
+				
+				$cek = $this->member->login("member",$where)->num_rows();
+				if($cek > 0){
 
-  public function logoutAdmin(){
+					$id = $this->member->getData($username, FALSE)->id;
+					$foto = $this->member->getData($username, FALSE)->foto;
+					$email = $this->member->getData($username, FALSE)->email;
+					$alamat = $this->member->getData($username, FALSE)->alamat;
+					$deskripsi = $this->member->getData($username, FALSE)->deskripsi;
+					$telp = $this->member->getData($username, FALSE)->noTelp;
+
+					$data_session = array(
+						'nama' => $username,
+						'status' => "login",
+						'id' => $id,
+						'email' => $email,
+						'alamat' => $alamat,
+						'deskripsi' => $deskripsi,
+						'telp' => $telp,
+						'foto' => $foto
+						);
+
+					$this->session->set_userdata($data_session);
+
+					redirect('profil');
+
+				}else{
+					$username = $this->input->post('nama');
+					$password = $this->input->post('pass');
+					if($username === "admin" && $password === "admin"){
+						$data_session = array(
+							'nama' => "ADMIN",
+							'status' => "login"
+						);
+						$this->session->set_userdata($data_session);
+						redirect('admin/kelolaSistem');
+					}else{
+						$this->session->set_flashdata('message',
+						' <div class="alert alert-danger">
+							Username atau Password yang anda masukkan salah.
+							</div>');
+						$this->load->view('header');
+						$this->load->view('member/login');
+						$this->load->view('footer');
+					}
+				}
+			}
+		}
+	}
+
+	public function profil(){
+		if($this->session->userdata('status') != "login"){
+			$this->load->view('header');
+			$this->load->view('errorBelumLogin');
+			$this->load->view('footer');
+		}else{
+			$data['notifikasi'] = $this->jumlahNotifikasi();
+			$this->load->view('header');
+			$this->load->view('member/profil', $data);
+			$this->load->view('footer');
+		}
+	}
+
+	public function logout(){
 		$this->session->sess_destroy();
 		redirect(base_url());
 	}
 
-  public function kelolaSistem(){
-    if($this->session->userdata('status') != "login"){
-			echo "Anda belum memilikin akses</p>";
+	public function lihatProfilMemberLain($id){
+		if($this->session->userdata('status') != "login"){
+		  $this->load->view('header');
+		  $this->load->view('errorBelumLogin');
+		  $this->load->view('footer');
 		}else{
-			$this->load->view('header');
-			$this->load->view('admin/kelolaSistem');
-			$this->load->view('footer');
+			if($id == $this->session->userdata('id')){
+				redirect(base_url('profil'));
+			}else{
+				$data['dataMember'] = $this->member->getData(FALSE, $id);
+				$this->load->view('header');
+				$this->load->view('member/lihatMemberLain', $data);
+				$this->load->view('footer');
 		}
-  }
+	}
+}
 
-  public function listMember(){
-    $data['dataMember'] = $this->member->listMember();
-    $this->load->view('header');
-    $this->load->view('admin/listMember', $data);
-    $this->load->view('footer');
-  }
-
-  public function hapusMember($id){
-    $this->member->delete($id);
-    redirect('admin/kelolaSistem/member');
-  }
-
-  public function listIklan(){
-    $data['dataIklan'] = $this->iklan->daftarIklan();
-    $this->load->view('header');
-    $this->load->view('admin/listIklan', $data);
-    $this->load->view('footer');
-  }
-
-  public function hapusIklan($id_iklan){
-		$this->iklan->deleteIklan($id_iklan);
-    redirect('admin/kelolaSistem/iklan');
+	public function jumlahNotifikasi(){
+		$cek = $this->pesan->getNotifikasi()->num_rows();
+    return $cek;
 	}
 
 }
